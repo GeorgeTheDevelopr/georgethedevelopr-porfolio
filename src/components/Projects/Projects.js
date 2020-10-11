b44b0c3cb0ae7d0d73cee7476d1a584d1505a8b0
George Brown <sumthnabtgeorge@gmail.com>
2020-10-10 23:48:28 -0400

    made last final changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -13,7 +13,7 @@ export default class Projects extends React.Component{
           <div className='project project_1'>
             <h3 className='project_title'>Music Quiz App</h3>
             <img src={musicapp} className="p1_img" alt="Quiz App Screenshot"></img>
-            <p className='project_desc'>Simple single page web app that generates five music related questions, based on chosen difficulty (easy, medium or hard). Inteded for those who wants to challenge their music knowledge across all genres.</p>
+            <p className='project_desc'>Simple single page web app that generates five music related questions, based on chosen difficulty (easy, medium or hard). Inteded for those who wants to challenge their music knowledge across all genres. Built simply as a fun project from a music lover's perspective.</p>
             <p className='tech_stack'>Stack: HTML5, CSS, JavaScript and JQuery.</p>
             <div className='projectLinks_container'>
               <p><a href="https://github.com/thinkful-ei-panda/Quiz-App-George-Rebecca" className="projectLinks" target="_blank" rel='noopener noreferrer'>Repo</a></p>
@@ -24,7 +24,7 @@ export default class Projects extends React.Component{
           <div className='project project_2'>
             <h3 className='project_title'>TransActionary</h3>
             <img src={transactionary} className="p2_img" alt="TransActionary Screenshot"></img>
-            <p className='project_desc'>Full Stack Web Application that allows users to create "transaction lists". This app takes a list of items (transaction) and assign them to vendor (online or in person)</p>
+            <p className='project_desc'>Full Stack Web Application that allows users to create "transaction lists". This app takes a list of items (transaction) and assign them to vendor (online or in person). Intended for those who quickly wants a way to save and view their transaction. Built with my mom in mind, as she still collects paper receipts, and over time, they tend to fade out.</p>
             <p className='tech_stack'>Stack: HTML5, CSS, JavaScript, PostgreSQL, Express.js, React.js, Node.js.</p>
             <div className='projectLinks_container'>
               <p><a href="https://github.com/GeorgeTheDevelopr/TransActionary-Client" className="projectLinks" target="_blank" rel='noopener noreferrer'>Repo</a></p>
@@ -34,8 +34,8 @@ export default class Projects extends React.Component{
 
           <div className='project project_3'>
           <h3 className='project_title'><pre>BuddyUp       </pre></h3>
-            <img src={buddyup} className="p2_img" alt="TransActionary Screenshot"></img>
-            <p className='project_desc'>Full Stack Web Application that allows you to organize, join and create activities in your area.</p>
+            <img src={buddyup} className="p2_img" alt="BuddyUp Screenshot"></img>
+            <p className='project_desc'>Full Stack Web Application that allows you to organize, join and create activities in your area. Intended for those who want to safely plan activies where anyone can tag along (with approval). Built as a group project with a team of 4. We all felt this would be a great idea for when the government slowly began reopening the majority of public spaces.</p>
             <p className='tech_stack'>Stack: HTML5, CSS, JavaScript, PostgreSQL, Express.js, React.js, Node.js.</p>
             <div className='projectLinks_container'>
               <p><a href="https://github.com/thinkful-ei-panda/buddyup-client/" className="projectLinks" target="_blank" rel='noopener noreferrer'>Repo</a></p>
